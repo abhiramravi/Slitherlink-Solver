b7ec1909d3de63c5c1fc34f26c5f12ecc55cdad2
Sabarinath N P <npsabari@gmail.com>
2012-10-10 22:06:28 +0530

    Adding the Gui Class Package

---
+++ iitm/api/ai/project/gui/Coordinate.java
@@ -0,0 +1,31 @@
+package iitm.api.ai.project.gui;
+
+public class Coordinate {
+
+	private int xpos, ypos;
+	
+	public Coordinate(int x, int y){
+		this.setX(x);
+		this.setY(y);
+	}
+	
+	/*
+	 * Getter Methods
+	 */
+	public int getX() {
+		return this.xpos;
+	}
+	public int getY(){
+		return this.ypos;
+	}
+	
+	/*
+	 * Setter Methods
+	 */
+	public void setX(int x){
+		this.xpos = x;
+	}
+	public void setY(int y){
+		this.ypos = y;
+	}
+}
